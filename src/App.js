1b54b975cf9ac162ada232564fa0c31f78ba8b6c
Nathan Huber <Nathan_Huber@yahoo.com>
2020-03-22 00:34:31 -0500

    Work on NavBar

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import { Header } from './components/Header';
+import { NavBar } from './components/NavBar';
+
+function App() {
+  return (
+    <div>
+
+     <NavBar />
+
+
+     <Header />
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import '../CSS/NavBar.css';
+
+export const NavBar = () => {
+  return (
+    <div class="navbar">
+      <div class="logo">
+
+      </div>
+      <a class="btn">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+      <div class="menu">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#works">Works</a>
+        <a href="#contact">Contact</a>
+      </div>
+
+    </div>
+
+
+  )
+}
