f6236b4158c3954cf177006ad9d369bcc4f511e4
Nathan Huber <Nathan_Huber@yahoo.com>
2020-03-27 00:03:05 -0500

    Made a few more updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,6 +8,20 @@ export const Contact = () => {
           <div className="col-md-12">
             <h3>Contact Me</h3>
             <p className="lead">Leave a message below if you have any questions or comments.</p>
+            <form>
+              <div class="form-group">
+                <label for="formGroupExampleInput"></label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Name"/>
+              </div>
+              <div class="form-group">
+                <label for="formGroupExampleInput2"></label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Email Address"/>
+              </div>
+              <div class="form-group">
+                <label for="exampleFormControlTextarea1"></label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Message" rows="3"></textarea>
+               </div>
+            </form>
           </div>
         </div>    
        
