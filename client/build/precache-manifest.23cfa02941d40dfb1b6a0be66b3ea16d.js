8425fb120b17f084a9121d5f2f63f62481b2a687
Nathan Huber <Nathan_Huber@yahoo.com>
2020-05-24 01:15:40 -0500

    another deploy attempt

---
+++ client/build/precache-manifest.23cfa02941d40dfb1b6a0be66b3ea16d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d882569b5e78887c3ee6ce7eed3c3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff115356a5be086c3358",
+    "url": "/static/css/main.c5433abb.chunk.css"
+  },
+  {
+    "revision": "ae3b32dac6566947cfeb",
+    "url": "/static/js/2.6462e4f8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6462e4f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff115356a5be086c3358",
+    "url": "/static/js/main.c5ce4ef4.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "6109c3ffdf9deae3da77b0fde0ff53bc",
+    "url": "/static/media/background.6109c3ff.jpg"
+  }
+]);
