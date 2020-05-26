4d2ff2733b61ff0cb9eef257e788c2643f5249c4
Nathan Huber <Nathan_Huber@yahoo.com>
2020-05-25 23:41:27 -0500

    Edited server.js & package.json files, trying to deploy to Heroku.

--- server.js
+++ server.js
@@ -17,8 +17,12 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static( 'client/build' ));
+
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+  });
 }
 
 
